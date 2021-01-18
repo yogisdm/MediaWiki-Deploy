20b64decc806352ab74c1918550e168030b3ef88
yogisdm <31575108+yogisdm@users.noreply.github.com>
2021-01-18 10:41:29 +0530

    Updated code - added additional variables

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "mediawiki_ip" {
   description = "Reserved IP for mediawiki server"
   default     = "eipalloc-03b72f259d374c443"
 }
+
+variable "subnet_ID" {
+   description = "Subnet ID"
+  default     = "subnet-0698441978a443411"
+  }
+
+variable "security_group_ID" {
+   description = "Security group ID"
+  default     = "sg-0d3c94d96b36416e7"
+  }
